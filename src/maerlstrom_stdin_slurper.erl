0a68d92a05a18d1cb51d63677b331023d2964c01
Peter Kang <peterjkang@gmail.com>
2024-06-05 23:13:59 -0400

    Holy shiz it reads from a file

--- src/maerlstrom_stdin_slurper.erl
+++ src/maerlstrom_stdin_slurper.erl
@@ -7,8 +7,22 @@
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-init(_Args) ->
+init([]) ->
+    spawn_link(fun() -> start_loop() end),
     {ok, []}.
+
+start_loop() ->
+    {ok, Fd} = file:open("asdf.txt", [read, raw]),
+    loop({Fd}).
+
+loop({Fd}) ->
+    case file:read_line(Fd) of
+        {ok, Data} ->
+            io:format("GOT LINE: ~s", [Data]),
+            loop({Fd});
+        eof ->
+            loop({Fd})
+    end.
 
 handle_call(hello, _From, _State) ->
     io:format("Hello~n"),
